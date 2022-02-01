5a19750960aee55c2d808fc50aa33adeeebd300d
voleksav <oleksa.vasylenko@gmail.com>
2022-02-01 21:25:48 +0200

    Move search function out

--- ifa-fetcher/main.py
+++ ifa-fetcher/main.py
@@ -3,6 +3,21 @@ import entities
 import fetcher
 
 
+def search_ingredient(
+    ingredient: str, ingredient_sample: list[str]
+) -> tuple[str, list[str]]:
+    if ingredient in ingredient_sample:
+        print("Found exact match:", ingredient)
+        return ingredient, []
+
+    print("Trying to find partial match...")
+    match = []
+    for item in ingredient_sample:
+        if ingredient in item:
+            match.append(item)
+    return "", match
+
+
 def main() -> None:
     items_to_find = csv_input.read_csv("../files/example_input.csv")
     report = entities.SearchReport()
@@ -11,18 +26,10 @@ def main() -> None:
         report_unit = entities.SearchReportUnit(SKU=sku)
         for ingr in ingredients:
             findings = fetcher.search(ingr)
-            if ingr in findings:
-                print("Found exact match:", ingr)
-                report_unit.exact_match.append(ingr)
-            else:
-                print("Trying to find partial match...")
-                match = []
-                for item in findings:
-                    if ingr in item:
-                        match.append(item)
-                if match:
-                    print("Partial match:", ingr, match)
-                    report_unit.partial_match[ingr] = match
+            exact_match, partial_match = search_ingredient(ingr, findings)
+            exact_match and report_unit.exact_match.append(exact_match)
+            if partial_match:
+                report_unit.partial_match[ingr] = partial_match
 
         report.units.append(report_unit)
     print(report.as_primitive())
