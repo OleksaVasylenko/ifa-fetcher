ed609d9e311bb31bb9081abc5b306380761e859a
voleksav <oleksa.vasylenko@gmail.com>
2022-02-13 02:03:59 +0200

    Add time measuring

--- src/ifa_fetcher/__main__.py
+++ src/ifa_fetcher/__main__.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from collections import OrderedDict
 
 from .csv_tools import read_csv, write_csv
@@ -16,7 +17,9 @@ def main() -> None:
     items_to_find = read_csv(in_file_path)
     ifa_client = create_ifa_client()
     ifa_search_service = IFAReportService(ifa_client)
+    start = time.time()
     report = ifa_search_service.build_report(items_to_find)
+    print("report generation took seconds:", time.time() - start)
     write_csv(report, out_file_path)
 
 
