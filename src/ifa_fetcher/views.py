4fa8e39053077a21f289ba5bf9b012e205de4f14
voleksav <oleksa.vasylenko@gmail.com>
2022-02-23 19:34:17 +0200

    Add rather useless page for checking the ingredients

--- src/ifa_fetcher/views.py
+++ src/ifa_fetcher/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask.typing import ResponseReturnValue
 
 fetcher = Blueprint("fetcher", __name__)
@@ -6,4 +6,4 @@ fetcher = Blueprint("fetcher", __name__)
 
 @fetcher.route("/fetcher", methods=("GET", "POST"))
 def fetcher_view() -> ResponseReturnValue:
-    return "OK"
+    return render_template("fetcher/fetcher.html")
