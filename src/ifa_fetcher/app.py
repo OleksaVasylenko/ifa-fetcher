f843fb9deeca2431791c7d798093207102968846
voleksav <oleksa.vasylenko@gmail.com>
2022-02-23 19:50:01 +0200

    Add rule to show ingredient fetcher page on /

--- src/ifa_fetcher/app.py
+++ src/ifa_fetcher/app.py
@@ -6,12 +6,9 @@ from flask import Flask
 def create_app() -> Flask:
     app = Flask(__name__)
 
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
-
-    from .views import fetcher
+    from .views import fetcher, fetcher_view
 
     app.register_blueprint(fetcher)
+    app.add_url_rule("/", view_func=fetcher_view)
 
     return app
